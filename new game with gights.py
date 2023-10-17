9d6c6cfa6004497a6d84b77520e5cb2d0da22028
EASYPANDAYT <145465554+EASYPANDAYT@users.noreply.github.com>
2023-10-17 17:47:08 +0300

    Add files via upload

--- new game with gights.py
+++ new game with gights.py
@@ -3,12 +3,13 @@ player_name = 'Вася Питонов'
 player_level = 10
 player_xp = 0
 player_hp = 100 * player_level
-player_money = 0
+player_money = 5
 
 while True:
-    print ('1 - начать сражение')
-    print ('2 - сыграть в кости')
-    print ('3 - уехать')
+    print ('1 - Начать сражение')
+    print ('2 - Сыграть в кости')
+    print ('3 - Отправиться в трактир')
+    print ('4 - Уехать')
     retry = input ('Введите номер ответа и нажмите Enter ')
     if retry == '1' :
         enemy_name = 'Жран Кривой'
@@ -30,6 +31,10 @@ while True:
                     player_level += player_xp // 10
                     player_xp += enemy_level % 10
                     print (player_name, 'получил', player_level, 'уровень')
+                    player_hp = player_hp
+                #hp+= 100 * lvl lvlup+1
+                    #todo добавить здоровья по штуке за каждый лвлап х 100
+                    
                 break
             enemy_attack = randint (0, 10) * enemy_level
             player_hp -= enemy_attack
@@ -39,34 +44,63 @@ while True:
                 print( player_name, 'погиб')
                 break
     elif retry == '2':
-        print ('У', player_name , player_money , 'денег')
-        bet = input ('Сколько поставить: ')
-        if bet <= 0 and bet > player_money :
-            print ('Ставка недействительна')
-        else :
-            bet = randint (1, 2)
-        if bet == 1:
-            print ('Вы победили')
-            player_money = player_money * 2
-            print ('У тебя осталось', player_money, 'денег')
-        elif player_2 > player_1:
-            print ('Компьютер победил')
-            chips = chips - 25
-            print ('У тебя осталось', chips, 'фишек')
-        else :
-            print ('Ничья')
-        
+        while True:
+            print ('---------------------------')
+            print ('1 - Сделать ставку')
+            print ('2 - Уехать')
+            casino = input ('Введите номер ответа и нажмите Enter: ')
+            print ('---------------------------')
+            if casino == '2' :
+                print (player_name, 'уехал')
+                break
+            if player_money <= 0 :
+                print ('У', player_name, 'недостаточно денег')
+                break
+            print ('У', player_name, player_money ,'денег')
+            bet =int ( input ('Сколько поставить?: '))
+            if bet > player_money :
+                print ('У', player_name, 'нет столько денег')
+                continue
+            if bet <= 0 :
+                print ('Ставка должна быть больше 0')
+                continue
+            player_score = randint (2, 12)
+            enemy_score = randint (2, 12)
+            print (player_name, 'выбросил', player_score)
+            print ('Соперник выбросил', enemy_score)
+            if player_score > enemy_score :
+                player_money += bet
+                print (player_name, 'выиграл', bet)
+                print ('У', player_name, player_money ,'денег')
+            if player_score < enemy_score :
+                player_money -= bet
+                print (player_name, 'проиграл', bet)
+                print ('У', player_name, player_money ,'денег')
+            if player_score == enemy_score :
+                print ('Ничья')
+#магазин
+    elif retry == '3':
+        while True :
+            print ('---------------------------')
+            print ('1 - Купить зелье восстановления (10 монет)')
+            print ('2 - Поговорить с торговцем')
+            print ('3 - Уехать')
+            tavern = input ('Введите номер ответа и нажмите Enter: ')
+            print ('---------------------------')
+            if tavern == '1' :
+                
+                
 
 
 
 
 
-
-
-
-    elif retry == '3' :
+         if casino == '3' :
+                        print (player_name, 'ушел из трактира')
+                        break
+    elif retry == '4' :
         print (player_name, 'уехал')
         break
-print ('Бой окончен')
+print ('Игра окончена')
 
 
